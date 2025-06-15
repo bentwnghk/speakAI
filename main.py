c692cdc12ef320e6871f0d7fedce4bf0c76703e8
Ben <mr.ng@mystudies.net>
2025-06-15 09:36:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ import pytz
 
 MINIMAX_CANTONESE_VOICE_MAPPINGS = {
     "female-1": "English_captivating_female1",
-    "male-1": "Cantonese_Narrator",
+    "male-1": "English_magnetic_voiced_man",
     "female-2": "English_AnimeCharacter",
     "male-2": "English_Comedian",
 }
