bef71b50c60348a7382af08c72104a1f33622044
Ben <mr.ng@mystudies.net>
2025-06-15 11:25:47 +0800

    Correctly calculate cost for Chinese and Cantonese podcasts

--- main.py
+++ main.py
@@ -618,8 +618,13 @@ def generate_audio(
 
     total_duration = time.time() - start_time
     tts_cost = (characters / 1_000_000) * 60
-    gr.Info(f"🎉 Podcast generation complete! Total time: {total_duration:.2f} seconds.")
-    gr.Info(f"💸 This podcast generation costs {tts_cost:.2f} US dollars.")
+    if language in ["Chinese", "Cantonese"]:
+        tts_cost *= 2
+        gr.Info(f"🎉 Podcast generation complete! Total time: {total_duration:.2f} seconds.")
+        gr.Info(f"💸 This podcast generation costs US${tts_cost:.2f}.")
+    else:
+        gr.Info(f"🎉 Podcast generation complete! Total time: {total_duration:.2f} seconds.")
+        gr.Info(f"💸 This podcast generation costs US${tts_cost:.2f}.")
 
     # Prepare podcast title for history
     # Get current time in UTC
