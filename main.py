b6fe48bc3f2d07c4b094d5de8d6a100bb4d812fe
Ben <mr.ng@mystudies.net>
2025-06-16 08:13:48 +0800

    Change audio sample rate to 16k

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def get_mp3_minimax(text: str, voice: str, group_id: str = None, api_key: str = 
         "pitch": 0
       },
       "audio_setting":{
-        "sample_rate": 44100,
+        "sample_rate": 16000,
         "bitrate": 128000,
         "format": "mp3",
         "channel": 1
